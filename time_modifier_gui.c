7ace31ff2fd6ab6e0b308ff447578cfbd3572317
gtschieder <32381262+gtschieder@users.noreply.github.com>
2018-11-30 13:29:45 -0500

    removed extra whitespace

--- time_modifier_gui.c
+++ time_modifier_gui.c
@@ -73,7 +73,6 @@ int main(int argc, char *argv[])
     ok_button = GTK_WIDGET(gtk_builder_get_object(builder, "ok_button"));
     apply_button = GTK_WIDGET(gtk_builder_get_object(builder, "apply_button"));
 
-
     g_object_unref(builder);
  
     gtk_widget_show(window);                
@@ -82,7 +81,6 @@ int main(int argc, char *argv[])
     return 0;
 }
  
-
 // called when window is closed
 void on_window_main_destroy()
 {
